9b54ed6d77407778ba234ead012b47f6bf1d6fec
prafullapal <prafullpal18@gmail.com>
2024-02-25 11:34:36 +0530

    routes updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,14 @@ const router = express.Router();
 // Import individual route modules
 const healthCheckRoutes = require("./healthcheck.routes");
 const userRoutes = require("./user.routes");
+const shopRoutes = require("./shop.routes");
+const productRoutes = require("./product.routes");
+const categoryRoutes = require("./category.routes");
 
 router.use("/health-check", healthCheckRoutes);
 router.use("/users", userRoutes);
+router.use("/shops", shopRoutes);
+router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
